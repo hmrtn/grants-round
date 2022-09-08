101ace37f73b92cfd00b5577512ed2221dbc8e4c
hans <hmrtn@pm.me>
2022-09-07 21:06:19 -0700

    added: wip qv

--- packages/contracts/contracts/votingStrategy/QuadraticVotingStratgy.sol
+++ packages/contracts/contracts/votingStrategy/QuadraticVotingStratgy.sol
@@ -4,31 +4,38 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./IVotingStrategy.sol";
-import "../utils/MetaPtr.sol";
 
 contract QuadraticVotingStrategy is IVotingStrategy, ReentrancyGuard, ERC721 {
-    uint256 public immutable CREDITS_PER_BADGE;
-    uint256 public immutable CREDIT_COST;
-    uint256 public voterCount;
-    uint256 public totalWeight;
-    mapping(address => uint256) public credits;
-    mapping(uint256 => uint256) public votes;
-    MetaPtr public projectsMetaPtr;
-    mapping(uint256 => uint256) public scores;
+    struct Vote {
+        uint256 weight;
+        uint256 amount;
+    }
+    struct Set {
+        uint256[] ids;
+        mapping(uint256 => bool) is_in;
+    }
 
-    constructor() ERC721("VOTE", "VOTE") {
-        uint256  _credits = 1000;
-        CREDITS_PER_BADGE = _credits;
-        CREDIT_COST = 1e18 / _credits;
+    uint256 public immutable VOTE_CREDITS_PER_BADGE;
+    uint256 public immutable CREDIT_COST_PER_VOTE;
+
+    Set tallySet;
+    bytes public finalTally;
+    bytes[] public votesData;
+    uint256 public voterCount;
+
+    mapping(address => uint256) public credits;
+    mapping(uint256 => Vote) public votes;
+
+    constructor(uint256 _credits, uint256 _voteCost) ERC721("VOTE", "VOTE") {
+        VOTE_CREDITS_PER_BADGE = _credits;
+        CREDIT_COST_PER_VOTE = _voteCost;
     }
 
     function register(address registrant) public {
         _mint(registrant, voterCount++);
-        credits[registrant] = CREDITS_PER_BADGE * balanceOf(registrant);
+        credits[registrant] = VOTE_CREDITS_PER_BADGE * balanceOf(registrant);
     }
 
-    // hack 
-    uint256[] public hack; // A list of projects will be from the RoundManager - somehow?
     function vote(bytes[] calldata encodedVotes, address voterAddress)
         external
         override
@@ -37,26 +44,42 @@ contract QuadraticVotingStrategy is IVotingStrategy, ReentrancyGuard, ERC721 {
         require(balanceOf(voterAddress) > 0, "NOT_REGISTERED");
         require(credits[voterAddress] > 0, "NO_CREDITS");
         for (uint256 i = 0; i < encodedVotes.length; i++) {
-            (uint256 grantId, uint256 amount) = abi.decode(
+            (uint256 id, uint256 amount) = abi.decode(
                 encodedVotes[i],
                 (uint256, uint256)
             );
-            hack.push(grantId);
-            uint256 cost = amount * amount;
+            uint256 cost = amount * amount * CREDIT_COST_PER_VOTE;
             require(cost <= credits[voterAddress], "INSUFFICIENT_CREDITS");
             credits[voterAddress] -= cost;
-            votes[grantId] += amount;
-            totalWeight += cost;
+            votesData.push(abi.encode(id, cost, amount));
         }
     }
 
-    function tally() external nonReentrant returns(uint256[] memory) {
-        uint256[] memory x;
-        for (uint256 i = 0; i < hack.length; i++) {
-            scores[hack[i]] = votes[hack[i]] / totalWeight;
-            x[i] = scores[hack[i]];
+    function tally() external nonReentrant {
+        for (uint256 i = 0; i < votesData.length; i++) {
+            (uint256 id, uint256 weight, uint256 amount) = abi.decode(
+                votesData[i],
+                (uint256, uint256, uint256)
+            );
+            if (!tallySet.is_in[id]) {
+                tallySet.ids.push(id);
+                tallySet.is_in[id] = true;
+            }
+            votes[id].weight += weight;
+            votes[id].amount += amount;
         }
-        return x; 
+
+        uint256[] memory ids = new uint256[](tallySet.ids.length);
+        uint256[] memory weights = new uint256[](tallySet.ids.length);
+        uint256[] memory amounts = new uint256[](tallySet.ids.length);
+
+        for (uint256 j = 0; j < tallySet.ids.length; j++) {
+            ids[j] = tallySet.ids[j];
+            weights[j] = votes[tallySet.ids[j]].weight;
+            amounts[j] = votes[tallySet.ids[j]].amount;
+        }
+
+        finalTally = abi.encode(ids, weights, amounts);
     }
 
     function _transfer(
@@ -64,6 +87,6 @@ contract QuadraticVotingStrategy is IVotingStrategy, ReentrancyGuard, ERC721 {
         address,
         uint256
     ) internal pure override {
-        revert("Badge: SOULBOUND");
+        revert("*~ SOULBOUND ~*");
     }
 }
